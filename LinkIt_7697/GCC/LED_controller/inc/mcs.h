216c4ba9d0d413702c6517abe78882df4ea4a54f
dariachen <smartexist@gmail.com>
2017-06-01 15:35:53 +0800

    add 7697 gcc example

---
+++ LinkIt_7697/GCC/LED_controller/inc/mcs.h
@@ -0,0 +1,24 @@
+#define _MCS_H_
+
+#define MCS_TCP_INIT_ERROR -1
+#define MCS_TCP_SOCKET_INIT_ERROR 0x1
+#define MCS_TCP_DISCONNECT 0x2
+#define MCS_MAX_STRING_SIZE 200
+
+
+
+//#define HTTPS_MTK_CLOUD_URL_COM "http://192.168.1.241:3000/api/devices/"
+//#define HOST "com"
+
+typedef void (*mcs_tcp_callback_t)(char *);
+typedef void (*mcs_mqtt_callback_t)(char *);
+
+void mcs_upload_datapoint(char *,char *api_server, int api_port, char *device_id, char *device_key);
+void mcs_mqtt_upload_datapoint(char* channel, char *value);
+int32_t mcs_tcp_init(void (*mcs_tcp_callback)(char *), char *websocket_server, uint16_t websocket_port, char *device_id, char *device_key);
+void mcs_mqtt_init(void (*mcs_mqtt_callback)(char *));
+
+/* utils */
+void mcs_split(char **arr, char *str, const char *del);
+void mcs_splitn(char ** dst, char * src, const char * delimiter, uint32_t max_split);
+char *mcs_replace(char *st, char *orig, char *repl);
